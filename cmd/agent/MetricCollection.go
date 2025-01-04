23e2dadb2a3deb66aad9470ccb5344c24ae7dd57
Fuonder <ranygi23@mail.ru>
2025-01-04 21:46:41 +0300

    iter 2. First demo version(no tests)

---
+++ cmd/agent/MetricCollection.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"runtime"
+	"sync"
+)
+
+type gauge float64
+
+func (t gauge) Type() string {
+	return "gauge"
+}
+
+type counter int64
+
+func (t counter) Type() string {
+	return "counter"
+}
+
+type MetricsCollection struct {
+	gMetrics map[string]gauge
+	cMetrics map[string]counter
+	mu       sync.Mutex
+}
+
+func NewMetricsCollection() (*MetricsCollection, error) {
+	mc := MetricsCollection{
+		gMetrics: make(map[string]gauge),
+		cMetrics: map[string]counter{"PollCount": 0},
+	}
+	return &mc, nil
+}
+
+func (mc *MetricsCollection) ReadValues() {
+	mc.mu.Lock()
+	var ms runtime.MemStats
+	runtime.ReadMemStats(&ms)
+	rNum := rand.Float64() * 100
+
+	mc.gMetrics["Alloc"] = gauge(ms.Alloc)
+	mc.gMetrics["BuckHashSys"] = gauge(ms.BuckHashSys)
+	mc.gMetrics["Frees"] = gauge(ms.Frees)
+	mc.gMetrics["GCCPUFraction"] = gauge(ms.GCCPUFraction)
+	mc.gMetrics["GCSys"] = gauge(ms.GCSys)
+	mc.gMetrics["HeapAlloc"] = gauge(ms.HeapAlloc)
+	mc.gMetrics["HeapIdle"] = gauge(ms.HeapIdle)
+	mc.gMetrics["HeapInuse"] = gauge(ms.HeapInuse)
+	mc.gMetrics["HeapObjects"] = gauge(ms.HeapObjects)
+	mc.gMetrics["HeapReleased"] = gauge(ms.HeapReleased)
+	mc.gMetrics["HeapSys"] = gauge(ms.HeapSys)
+	mc.gMetrics["LastGC"] = gauge(ms.LastGC)
+	mc.gMetrics["Lookups"] = gauge(ms.Lookups)
+	mc.gMetrics["MCacheInuse"] = gauge(ms.MCacheInuse)
+	mc.gMetrics["MCacheSys"] = gauge(ms.MCacheSys)
+	mc.gMetrics["MSpanInuse"] = gauge(ms.MSpanInuse)
+	mc.gMetrics["MSpanSys"] = gauge(ms.MSpanSys)
+	mc.gMetrics["Mallocs"] = gauge(ms.Mallocs)
+	mc.gMetrics["NextGC"] = gauge(ms.NextGC)
+	mc.gMetrics["NumForcedGC"] = gauge(ms.NumForcedGC)
+	mc.gMetrics["NumGC"] = gauge(ms.NumGC)
+	mc.gMetrics["OtherSys"] = gauge(ms.OtherSys)
+	mc.gMetrics["PauseTotalNs"] = gauge(ms.PauseTotalNs)
+	mc.gMetrics["StackInuse"] = gauge(ms.StackInuse)
+	mc.gMetrics["StackSys"] = gauge(ms.StackSys)
+	mc.gMetrics["Sys"] = gauge(ms.Sys)
+	mc.gMetrics["TotalAlloc"] = gauge(ms.TotalAlloc)
+	mc.cMetrics["PollCount"] = mc.cMetrics["PollCount"] + 1
+	mc.gMetrics["RandomValue"] = gauge(rNum)
+	log.Printf("PollCount:\t%d\n", mc.cMetrics["PollCount"])
+	mc.mu.Unlock()
+}
--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,79 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var (
+	pollInterval   = 2 * time.Second
+	reportInterval = 10 * time.Second
+	mc, _          = NewMetricsCollection()
+)
+
+func updateValues() {
+	for {
+		log.Println("Updating metrics collection")
+		mc.ReadValues()
+		time.Sleep(pollInterval)
+	}
+}
+
+func main() {
+	go updateValues()
+	for {
+		log.Println("Sending metrics collection")
+		mc.mu.Lock()
+		err := SendMetrics()
+		if err != nil {
+			return
+		}
+		mc.mu.Unlock()
+		time.Sleep(reportInterval)
+	}
+
+}
+
+func SendMetrics() error {
+	for name, value := range mc.gMetrics {
+		url := "http://localhost:8080/update/"
+		url += value.Type() + "/" + name + "/" + strconv.FormatFloat(float64(value), 'f', -1, 64)
+		request, err := http.NewRequest(http.MethodPost, url, nil)
+		if err != nil {
+			return fmt.Errorf("error creating request: %v", err)
+		}
+		request.Header.Add("Content-Type", "text/plain")
+		client := &http.Client{}
+		resp, err := client.Do(request)
+		defer resp.Body.Close()
+		if err != nil {
+			return fmt.Errorf("could not send request: %w", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("received status code: %d", resp.StatusCode)
+		}
+
+	}
+	for name, value := range mc.cMetrics {
+		url := "http://localhost:8080/update/"
+		url += value.Type() + "/" + name + "/" + strconv.FormatInt(int64(value), 10)
+		request, err := http.NewRequest(http.MethodPost, url, nil)
+		if err != nil {
+			return fmt.Errorf("error creating request: %v", err)
+		}
+		request.Header.Add("Content-Type", "text/plain")
+		client := &http.Client{}
+		resp, err := client.Do(request)
+		defer resp.Body.Close()
+		if err != nil {
+			return fmt.Errorf("could not send request: %w", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("received status code: %d", resp.StatusCode)
+		}
+	}
+	return nil
+}
