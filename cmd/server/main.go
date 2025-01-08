5f410d55803812f6056c8167d06eca731af4412b
Fuonder <ranygi23@mail.ru>
2025-01-08 04:30:29 +0300

    refactor + server iter3 fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -18,10 +20,156 @@ func main() {
 	}
 }
 
+func valueHandler(rw http.ResponseWriter, r *http.Request) {
+	mType := chi.URLParam(r, "mType")
+	mName := chi.URLParam(r, "mName")
+
+	if mType == "gauge" {
+		value, err := ms.GetGaugeMetric(mName)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusNotFound)
+		} else {
+			io.WriteString(rw, mName+" "+strconv.FormatFloat(float64(value), 'f', -1, 64))
+			return
+		}
+	} else if mType == "counter" {
+		value, err := ms.GetCounterMetric(mName)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusNotFound)
+		} else {
+			io.WriteString(rw, mName+" "+strconv.FormatInt(int64(value), 10))
+			return
+		}
+	}
+}
+func updateHandler(rw http.ResponseWriter, r *http.Request) {
+	mType := chi.URLParam(r, "mType")
+	mName := chi.URLParam(r, "mName")
+	mValue := chi.URLParam(r, "mValue")
+
+	if mType == "gauge" {
+		value, _ := checkTypeGauge(mValue)
+		ms.AppendGaugeMetric(mName, value)
+		rw.WriteHeader(http.StatusOK)
+	} else if mType == "counter" {
+		value, _ := checkTypeCounter(mValue)
+		ms.AppendCounterMetric(mName, value)
+		rw.WriteHeader(http.StatusOK)
+	} else {
+		http.Error(rw, "Internal server error", http.StatusInternalServerError)
+	}
+}
+func rootHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "text/html")
+	var metricList []string
+	for name, value := range ms.gMetric {
+		metricList = append(metricList, fmt.Sprintf("%s %s",
+			name,
+			strconv.FormatFloat(float64(value), 'f', -1, 64)))
+	}
+	for name, value := range ms.cMetric {
+		metricList = append(metricList, fmt.Sprintf("%s %s",
+			name,
+			strconv.FormatInt(int64(value), 10)))
+	}
+	io.WriteString(rw, strings.Join(metricList, ", "))
+}
+
+func checkMethod(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost && r.Method != http.MethodGet {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+func checkContentType(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Content-Type") != "text/plain" &&
+			r.Header.Get("Content-Type") != "text/plain; charset=UTF-8" {
+			http.Error(w, "invalid content type", http.StatusBadRequest)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+func checkMetricType(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mType := chi.URLParam(r, "mType")
+		if mType != "counter" && mType != "gauge" {
+			http.Error(w, "invalid metric type", http.StatusBadRequest)
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func checkMetricName(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		mName := chi.URLParam(r, "mName")
+		if strings.TrimSpace(mName) == "" {
+			http.Error(rw, "metric name is required", http.StatusNotFound)
+		} else {
+			next.ServeHTTP(rw, r)
+		}
+	})
+}
+
+func checkMetricValue(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		mType := chi.URLParam(r, "mType")
+		mValue := chi.URLParam(r, "mValue")
+		var err error
+		if mType == "gauge" {
+			_, err = checkTypeGauge(mValue)
+		} else if mType == "counter" {
+			_, err = checkTypeCounter(mValue)
+		}
+		if err != nil {
+			http.Error(rw, "invalid metric value", http.StatusBadRequest)
+		} else {
+			next.ServeHTTP(rw, r)
+		}
+	})
+}
+
 func run() error {
 	srvConf, _ := newConfig("localhost", "8080")
 	log.Printf("Listening at %s\n", srvConf.fullAddr())
-	return http.ListenAndServe(srvConf.fullAddr(), http.HandlerFunc(metricHandler))
+	return http.ListenAndServe(srvConf.fullAddr(), metricRouter())
+}
+
+func metricRouter() chi.Router {
+	router := chi.NewRouter()
+	router.Use(checkMethod)
+	router.Use(checkContentType)
+	router.Get("/", rootHandler)
+	router.Route("/update", func(router chi.Router) {
+		router.Route("/{mType}", func(router chi.Router) {
+			router.Use(checkMetricType)
+			router.Route("/{mName}", func(router chi.Router) {
+				router.Use(checkMetricName)
+				router.Post("/", http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+					http.Error(rw, "incorrect metric value", http.StatusBadRequest)
+				}))
+				router.Route("/{mValue}", func(router chi.Router) {
+					router.Use(checkMetricValue)
+					router.Post("/", updateHandler)
+				})
+			})
+		})
+	})
+	router.Route("/value", func(router chi.Router) {
+		router.Route("/{mType}", func(router chi.Router) {
+			router.Use(checkMetricType)
+			router.Route("/{mName}", func(router chi.Router) {
+				router.Use(checkMetricName)
+				router.Get("/", valueHandler)
+			})
+		})
+	})
+	return router
 }
 
 func checkTypeGauge(value string) (gauge, error) {
--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -2,22 +2,40 @@ package main
 
 import (
 	"github.com/stretchr/testify/require"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestMetricHandler(t *testing.T) {
+func testRequest(t *testing.T, ts *httptest.Server,
+	method string, contentType string, path string) (*http.Response, string) {
+
+	req, err := http.NewRequest(method, ts.URL+path, nil)
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", contentType)
+	resp, err := ts.Client().Do(req)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	return resp, string(respBody)
+}
+
+func TestMetricRouter(t *testing.T) {
 	tests := []struct {
 		name        string
 		url         string
 		method      string
 		contentType string
 		want        int
+		wantResp    string
 	}{
 		{
 			name:        "PositiveGauge",
-			url:         "/update/gauge/gMetric/1.00",
+			url:         "/update/gauge/gMetric/1.01",
 			method:      http.MethodPost,
 			contentType: "text/plain",
 			want:        http.StatusOK,
@@ -86,20 +104,80 @@ func TestMetricHandler(t *testing.T) {
 			want:        http.StatusBadRequest,
 		},
 	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			//srvUrl := "http://localhost:8080"
-			request := httptest.NewRequest(test.method, test.url, nil)
-			request.Header.Set("Content-Type", test.contentType)
-			recorder := httptest.NewRecorder()
-			h := http.HandlerFunc(metricHandler)
-			h(recorder, request)
-			res := recorder.Result()
-			defer res.Body.Close()
 
-			require.Equal(t, test.want, res.StatusCode)
-
-		})
+	testsGet := []struct {
+		name        string
+		url         string
+		method      string
+		contentType string
+		want        int
+		wantResp    string
+	}{
+		{
+			name:        "PositiveGetValue",
+			url:         "/value/gauge/gMetric",
+			method:      http.MethodGet,
+			contentType: "text/plain",
+			want:        http.StatusOK,
+			wantResp:    "gMetric 1.01",
+		},
+		{
+			name:        "PositiveGetAllValues",
+			url:         "/",
+			method:      http.MethodGet,
+			contentType: "text/plain",
+			want:        http.StatusOK,
+			wantResp:    "gMetric 1.01, cMetric 2",
+		},
+		{name: "NegativeValue",
+			url:         "/value/gauge/negative",
+			method:      http.MethodGet,
+			contentType: "text/plain",
+			want:        http.StatusNotFound,
+			wantResp:    "metric with such key is not found: negative\n",
+		},
 	}
 
+	ts := httptest.NewServer(metricRouter())
+	defer ts.Close()
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			resp, _ := testRequest(t, ts, test.method, test.contentType, test.url)
+			require.Equal(t, test.want, resp.StatusCode)
+		})
+	}
+	for _, test := range testsGet {
+		t.Run(test.name, func(t *testing.T) {
+			resp, body := testRequest(t, ts, test.method, test.contentType, test.url)
+			require.Equal(t, test.want, resp.StatusCode)
+			require.Equal(t, test.wantResp, body)
+		})
+	}
+	//fmt.Println(ms) &{map[gMetric:1] map[cMetric:2] {0 0}}
+
+	//fmt.Println(ms)
+	//fmt.Println(ms)
+	//fmt.Println(testsGet)
+
 }
+
+//func TestMetricHandler(t *testing.T) {
+//
+//	for _, test := range tests {
+//		t.Run(test.name, func(t *testing.T) {
+//			//srvUrl := "http://localhost:8080"
+//			request := httptest.NewRequest(test.method, test.url, nil)
+//			request.Header.Set("Content-Type", test.contentType)
+//			recorder := httptest.NewRecorder()
+//			h := http.HandlerFunc(metricHandler)
+//			h(recorder, request)
+//			res := recorder.Result()
+//			defer res.Body.Close()
+//
+//			require.Equal(t, test.want, res.StatusCode)
+//
+//		})
+//	}
+//
+//}
