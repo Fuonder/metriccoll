e7c1343733e6ee04ea2ffeaa4fa0379910d5e300
Fuonder <ranygi23@mail.ru>
2025-01-08 04:44:10 +0300

    server fix content type checks v1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 	"strconv"
 	"strings"
 )
@@ -87,7 +86,8 @@ func checkMethod(next http.Handler) http.Handler {
 func checkContentType(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Content-Type") != "text/plain" &&
-			r.Header.Get("Content-Type") != "text/plain; charset=UTF-8" {
+			r.Header.Get("Content-Type") != "text/plain; charset=UTF-8" &&
+			r.Header.Get("Content-Type") != "text/plain; charset=utf-8" {
 			http.Error(w, "invalid content type", http.StatusBadRequest)
 		} else {
 			next.ServeHTTP(w, r)
@@ -185,70 +185,4 @@ func checkTypeCounter(value string) (counter, error) {
 		return 0, fmt.Errorf("wrong format %w", err)
 	}
 	return counter(converted), nil
-}
-
-func processData(URL *url.URL) int {
-	stringURL := URL.String()
-	urlParts := strings.Split(stringURL, "/")
-	urlPartsLen := len(urlParts)
-
-	log.Printf("URL length %d \n", urlPartsLen)
-	log.Println(urlParts)
-	if urlParts[1] != "update" || urlPartsLen < 5 {
-		return http.StatusNotFound
-	}
-	if urlParts[3] == "" {
-		return http.StatusNotFound
-	}
-
-	log.Println("Guessing types")
-	if urlParts[2] == "gauge" {
-		log.Println("Guessing type \"gauge\"")
-		log.Println("Guessing - YES")
-		log.Println("Checking value")
-		value, err := checkTypeGauge(urlParts[4])
-		if err != nil {
-			log.Println(err)
-			return http.StatusBadRequest
-		}
-		log.Println("Value - OK")
-		log.Println("Appending to storage")
-		ms.AppendGaugeMetric(urlParts[3], value)
-	} else if urlParts[2] == "counter" {
-		log.Println("Guessing type \"counter\"")
-		log.Println("Guessing - YES")
-		log.Println("Checking value")
-		value, err := checkTypeCounter(urlParts[4])
-		if err != nil {
-			log.Println(err)
-			return http.StatusBadRequest
-		}
-		log.Println("Value - OK")
-		log.Println("Appending to storage")
-		ms.AppendCounterMetric(urlParts[3], value)
-	} else {
-		log.Println("Unsupported type")
-		return http.StatusBadRequest
-	}
-	log.Println("Gauge: ", ms.gMetric)
-	log.Println("Counter: ", ms.cMetric)
-	return http.StatusOK
-}
-
-func metricHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Got request")
-	log.Println("Validating request")
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	log.Println("Method - OK")
-	if r.Header.Get("Content-Type") != "text/plain" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	log.Println("Content-Type - OK")
-	log.Println("Processing URL")
-	err := processData(r.URL)
-	w.WriteHeader(err)
 }
